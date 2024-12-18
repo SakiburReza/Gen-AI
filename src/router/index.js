900502836821e6b1ce57dbf727f5cab4547260de
Rafiul Abeer Khan <rafiulabeerkhan296@gmail.com>
2024-12-18 17:21:44 +0600

    Subscription page. back button needs to be responsive

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import OperativePage from '@/views/OperativePage.vue'
 import SignInComponent from '@/components/SignInComponent.vue'
 import SignUpComponent from '@/components/SignUpComponent.vue'
-
+import SubscriptionPage from '@/views/SubscriptionPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -26,11 +26,11 @@ const router = createRouter({
     //   name: 'profile',
     //   component: ProfilePage,
     // },
-    // {
-    //   path: '/subscription',
-    //   name: 'subscription',
-    //   component: SubscriptionPage,
-    // }
+    {
+      path: '/subscription',
+      name: 'subscription',
+      component: SubscriptionPage,
+    }
    
   ],
 })
