081744c0ffd2b68e744ff6fffac5def66bd24552
Roney <42718238+anvthe@users.noreply.github.com>
2024-12-18 15:47:32 +0600

    Manage Profile

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import OperativePage from '@/views/OperativePage.vue'
 import SignInComponent from '@/components/SignInComponent.vue'
 import SignUpComponent from '@/components/SignUpComponent.vue'
-import ProfilePage from '@/components/ProfilePage.vue'
+import ProfilePage from '@/views/ProfilePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
