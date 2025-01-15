dbced97089ae02978c7c17f199b738be4dc6d9fc
Rafiul Abeer Khan <rafiulabeerkhan296@gmail.com>
2025-01-15 11:44:57 +0600

    Buy more credit details container is now fully clickable

--- src/components/BuyMoreCreditsModal.vue
+++ src/components/BuyMoreCreditsModal.vue
@@ -118,25 +118,23 @@ onMounted(() => {
 
       <!-- Cards Container -->
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-6">
-        <div v-for="(feature, index) in plans" :key="index"
-          class="bg-tertiary p-4 rounded-lg flex flex-col justify-between shadow-sm h-355 items-end group hover:bg-blue-600 transition-all duration-300">
+        <button v-for="(feature, index) in plans" :key="index" @click="extraCreditBuy(feature)"
+          class="bg-tertiary p-4 rounded-lg flex flex-col justify-between shadow-sm h-355 items-end group hover:bg-blue-600 transition-all duration-300 w-full text-left">
           <div class="flex items-center justify-center w-full">
             <img src="/public/images/icon/StartIcon.svg" alt="Start Icon"
               class="w-4 h-5 ml-2 mb-14 mr-0.5 group-hover:filter group-hover:brightness-0 group-hover:invert">
-            <button @click="extraCreditBuy(feature)"
-              class="flex flex-col items-center p-4 rounded-lg transition-all duration-300">
-              <!-- Hover effect on the <p> tags based on the parent .group hover state -->
-              <p class="text-3xl md:text-xl font-bold text-gray-800 mt-2 group-hover:text-white">{{ feature.credits
-                }}</p>
-              <br>
-              <p class="text-lg md:text-md font-semibold text-ravenBlack mt-2 group-hover:text-white">${{ feature.price
-                }}
+            <div class="flex flex-col items-center p-4 rounded-lg transition-all duration-300">
+              <p class="text-3xl md:text-xl font-bold text-gray-800 mt-2 group-hover:text-white">{{ feature.credits }}
               </p>
-            </button>
+              <br>
+              <p class="text-lg md:text-md font-semibold text-ravenBlack mt-2 group-hover:text-white">
+                ${{ feature.price }}
+              </p>
+            </div>
           </div>
-        </div>
-
+        </button>
       </div>
+
 
 
     </div>
