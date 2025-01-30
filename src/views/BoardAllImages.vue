2471fca60e999a2445dfa9b67ed6b377923bb05f
Md. Sakibur Reza <sakiburrezajony@gmail.com>
2025-01-30 11:58:28 +0600

    Preview modal added at board

--- src/views/BoardAllImages.vue
+++ src/views/BoardAllImages.vue
@@ -4,14 +4,11 @@ import { imageUrl } from '@/utils/utils'
 import { onMounted, ref } from 'vue'
 import genAiService from '@/services/gen-ai'
 import { useRoute } from 'vue-router'
+import PreviewImageModal from '@/components/PreviewImageModal.vue'
 
-const images  = ref([])
+const images = ref([])
 const route = useRoute()
 const board = route.query.board
-
-
-
-
 
 onMounted(() => {
   boardImagesByName()
@@ -19,11 +16,10 @@ onMounted(() => {
 })
 
 const boardImagesByName = async () => {
-  
   try {
     const response = await genAiService.getBoardImagesByName(board)
     if (response.data.data?.length) {
-      images.value = response.data.data[0].images || [] 
+      images.value = response.data.data[0].images || []
     }
   } catch (error) {
     console.error('Error fetching board images:', error)
@@ -31,30 +27,54 @@ const boardImagesByName = async () => {
 }
 
 const collaborateBoardImagesByName = async () => {
-  
   try {
     const response = await genAiService.getCollaborateBoardsInfo(board)
     if (response.data.data?.length) {
-      images.value = response.data.data[0].images || [] 
+      images.value = response.data.data[0].images || []
     }
   } catch (error) {
     console.error('Error fetching board images:', error)
   }
 }
-
-
+//Functions to open/close modal
+const showPreviewModal = ref(false)
+const selectedImage = ref(null) 
+const closePreviewModal = () => {
+  showPreviewModal.value = false
+  selectedImage.value = null
+}
+const openPreviewModal = (mediaItem) => {
+  selectedImage.value = mediaItem
+  showPreviewModal.value = true
+}
 </script>
 
 <template>
   <DefaultLayout>
-    <div class="grid grid-cols-8 gap-2 p-2">
-        <img
-        v-for="(img, index) in images"
+    <div
+      class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3 rounded-t-2xl overflow-y-auto"
+    >
+      <div
+        v-for="(item, index) in images"
         :key="index"
-        class="rounded-lg border cursor-pointer w-50 h-50"
-        :src="imageUrl() + img.imageKey"
-        alt="Board Image"
-      />
+        class="relative overflow-hidden group"
+        :class="[item.orientation === 'P' ? 'row-span-2' : 'row-span-1']"
+      >
+        <!-- Render Image -->
+        <img
+          :src="imageUrl() + item.imageKey"
+          :alt="'Media ' + index"
+          class="h-full max-w-full w-full object-cover"
+          :class="[item.orientation === 'P' ? 'aspect-[3/4]' : 'aspect-[16/9]']"
+          @click="openPreviewModal(item)"
+        />
+      </div>
     </div>
+      <!-- Modal Component -->
+      <PreviewImageModal
+      :isOpen="showPreviewModal"
+      @close="closePreviewModal"
+      :image="selectedImage"
+    />
   </DefaultLayout>
 </template>
