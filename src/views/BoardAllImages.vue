54e9401e5538236a70a8c74f15356e28d65d5f32
Md. Sakibur Reza <sakiburrezajony@gmail.com>
2025-01-30 12:19:03 +0600

    Gallery layout done

--- src/views/BoardAllImages.vue
+++ src/views/BoardAllImages.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import DefaultLayout from '@/layout/DefaultLayout.vue'
 import { imageUrl } from '@/utils/utils'
 import { onMounted, ref } from 'vue'
@@ -10,6 +10,18 @@ const images = ref([])
 const route = useRoute()
 const board = route.query.board
 
+const media = ref<
+  {
+    url: string
+    type: 'image' | 'video'
+    orientation: 'P' | 'L'
+    isLiked: 'Y' | 'N'
+    isShared: 'Y' | 'N'
+    prompt: string,
+    owner:string
+  }[]
+>([])
+
 onMounted(() => {
   boardImagesByName()
   collaborateBoardImagesByName()
@@ -18,11 +30,23 @@ onMounted(() => {
 const boardImagesByName = async () => {
   try {
     const response = await genAiService.getBoardImagesByName(board)
-    if (response.data.data?.length) {
-      images.value = response.data.data[0].images || []
+    if (response.status && Array.isArray(response.data.data[0].images)) {
+      // Map data with type detection (image/video) for initial load
+      media.value = response.data.data[0].images.map((item) => ({
+        url: item.content,
+        type: item.type,
+        orientation: item.orientation,
+        prompt: item.prompt,
+        isLiked: item.like,
+        isShared: item.share,
+        owner: item.shareOwner
+      }))
+
+    } else {
+      console.error('Failed to fetch images: Invalid response format')
     }
   } catch (error) {
-    console.error('Error fetching board images:', error)
+    console.error('Error fetching images:', error)
   }
 }
 
@@ -44,6 +68,7 @@ const closePreviewModal = () => {
   selectedImage.value = null
 }
 const openPreviewModal = (mediaItem) => {
+  console.log('mediaItem', mediaItem)
   selectedImage.value = mediaItem
   showPreviewModal.value = true
 }
@@ -55,14 +80,14 @@ const openPreviewModal = (mediaItem) => {
       class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3 rounded-t-2xl overflow-y-auto"
     >
       <div
-        v-for="(item, index) in images"
+        v-for="(item, index) in media"
         :key="index"
         class="relative overflow-hidden group"
         :class="[item.orientation === 'P' ? 'row-span-2' : 'row-span-1']"
       >
         <!-- Render Image -->
         <img
-          :src="imageUrl() + item.imageKey"
+          :src="imageUrl() + item.url"
           :alt="'Media ' + index"
           class="h-full max-w-full w-full object-cover"
           :class="[item.orientation === 'P' ? 'aspect-[3/4]' : 'aspect-[16/9]']"
