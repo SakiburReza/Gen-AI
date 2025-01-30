9f211cc2ee11108dbf02d8ffa292fa8a487e6574
Ahmed Rifat <rifatbafsc@gmail.com>
2025-01-30 15:54:52 +0600

    user info added in board

--- src/views/BoardAllImages.vue
+++ src/views/BoardAllImages.vue
@@ -7,6 +7,7 @@ import { useRoute } from 'vue-router'
 import PreviewImageModal from '@/components/PreviewImageModal.vue'
 
 const images = ref([])
+const searchQuery = ref('')
 const route = useRoute()
 const board = route.query.board
 
@@ -17,8 +18,8 @@ const media = ref<
     orientation: 'P' | 'L'
     isLiked: 'Y' | 'N'
     isShared: 'Y' | 'N'
-    prompt: string,
-    owner:string
+    prompt: string
+    owner: string
   }[]
 >([])
 
@@ -34,14 +35,13 @@ const boardImagesByName = async () => {
       // Map data with type detection (image/video) for initial load
       media.value = response.data.data[0].images.map((item) => ({
         url: item.content,
-        type: item.type,
+        type: item.type || 'image',
         orientation: item.orientation,
         prompt: item.prompt,
         isLiked: item.like,
         isShared: item.share,
-        owner: item.shareOwner
+        owner: item.shareOwner,
       }))
-
     } else {
       console.error('Failed to fetch images: Invalid response format')
     }
@@ -62,7 +62,7 @@ const collaborateBoardImagesByName = async () => {
 }
 //Functions to open/close modal
 const showPreviewModal = ref(false)
-const selectedImage = ref(null) 
+const selectedImage = ref(null)
 const closePreviewModal = () => {
   showPreviewModal.value = false
   selectedImage.value = null
@@ -76,8 +76,58 @@ const openPreviewModal = (mediaItem) => {
 
 <template>
   <DefaultLayout>
+    <!-- search input -->
+    <div class="relative group w-full mt-2 p-2">
+      <img
+        src="/src/assets/icon/SearchIcon.svg"
+        alt="Search"
+        class="h-5 w-5 absolute top-1/2 ml-3 transform -translate-y-1/2 text-gray-500"
+      />
+      <input
+        v-model="searchQuery"
+        type="text"
+        placeholder="Search"
+        class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-0 focus:border-gray-300"
+      />
+    </div>
+    <!-- user information -->
+    <div class="flex items-center justify-center p-6">
+      <div class="w-full max-w-lg">
+        <div class="flex justify-between items-center">
+          <h2 class="text-xl font-semibold">{{ board }}</h2>
+          <div class="flex items-center gap-3">
+            <img src="/src/assets/icon/shareThreeDot.svg" alt="" />
+          </div>
+        </div>
+        <p class="text-gray-500 text-sm">{{ board.length }} Photos</p>
+        <div class="flex items-center gap-3 mt-2">
+          <img
+            src="https://via.placeholder.com/40"
+            alt="Profile"
+            class="w-10 h-10 border-2 border-gray-300"
+          />
+        </div>
+        <div class="flex mt-2">
+          <button class="flex flex-col items-center">
+            <img src="/src/assets/icon/organize.svg" alt="" />
+            <span class="text-sm text-gray-700">Organize</span>
+          </button>
+          <button class="flex flex-col items-center w-24">
+            <img src="/src/assets/icon/moreIdeas.svg" alt="" />
+            <span class="text-sm text-gray-700">More Ideas</span>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- filter and create board -->
+    <div class="flex justify-between p-2">
+      <div></div>
+      <div><img class="h-12 w-12" src="/src/assets/icon/filterIconWhite.svg" alt="" /></div>
+    </div>
+
     <div
-      class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3 rounded-t-2xl overflow-y-auto"
+      class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3 rounded-t-2xl mb-10 overflow-y-auto p-2"
     >
       <div
         v-for="(item, index) in media"
@@ -95,8 +145,8 @@ const openPreviewModal = (mediaItem) => {
         />
       </div>
     </div>
-      <!-- Modal Component -->
-      <PreviewImageModal
+    <!-- Modal Component -->
+    <PreviewImageModal
       :isOpen="showPreviewModal"
       @close="closePreviewModal"
       :image="selectedImage"
