ec3b8ae64b0edd958e48c215165f57d5659307de
Rafiul Abeer Khan <rafiulabeerkhan296@gmail.com>
2025-02-09 17:31:00 +0600

    Community page has now different component

---
+++ src/components/FollowingPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+</template>
