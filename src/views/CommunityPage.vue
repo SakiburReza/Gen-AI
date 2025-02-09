066801da1be86e458144b35219532dd3cafe8a6a
Md. Sakibur Reza <sakiburrezajony@gmail.com>
2025-02-09 17:47:28 +0600

    community clean

--- src/views/CommunityPage.vue
+++ src/views/CommunityPage.vue
@@ -8,8 +8,6 @@ import { useRoute } from 'vue-router'
 import { Divide } from 'lucide-vue-next'
 import FavouritePage from '@/components/FavouritePage.vue'
 import FollowingPage from '@/components/FollowingPage.vue'
-
-
 const searchQuery = ref('')
 const authTokenInfo = ref('')
 const isSaveBoardOpen = ref(false)
@@ -21,21 +19,6 @@ const selectedImage = ref(null)
 const showPreviewModal = ref(false)
 const loading = ref(false)
 const showMenu = ref(false)
-
-
-
-// const checkAuthentication = () => {
-//   authTokenInfo.value = localStorage.getItem('authToken')
-//   if (authTokenInfo.value) {
-//     return true
-//   }
-//   else {
-//     toastStore.error('Please login to continue')
-//     return false
-//   }
-// }
-
-
 const media = ref<
   {
     url: string
@@ -48,175 +31,8 @@ const media = ref<
     board: string
   }[]
 >([])
-
-// const filteredMedia = computed(() => {
-//   if (!searchQuery.value || searchQuery.value.trim() === '') {
-//     return media.value
-//   }
-//   return media.value.filter((item) => {
-//     const prompt = item.prompt ? item.prompt.toLowerCase() : ''
-//     return prompt.includes(searchQuery.value.toLowerCase())
-//   })
-// })
-
-
-// const closePreviewModal = () => {
-//   showPreviewModal.value = false
-//   selectedImage.value = null
-// }
-// const openPreviewModal = (mediaItem) => {
-//   if (!checkAuthentication()) return;
-//   selectedImage.value = mediaItem
-//   showPreviewModal.value = true
-// }
-
-// const showTurnIntoVideoModal = ref(false)
-// const closeTurnIntoVideoModal = () => {
-//   showTurnIntoVideoModal.value = false
-//   selectedImage.value = null
-// }
-// const openTurnIntoVideoModal = (mediaItem) => {
-//   if (!checkAuthentication()) return;
-//   selectedImage.value = mediaItem
-//   showTurnIntoVideoModal.value = true
-
-// }
-// const openSaveBoard = (mediaUrl) => {
-//   if (!checkAuthentication()) return;
-//   imageUrlData.value = mediaUrl;
-//   isSaveBoardOpen.value = true;
-// };
-
-// const openAddFriendModal = () => {
-//   if (!checkAuthentication()) return;
-// }
-
-// const closeSaveBoard = () => {
-//   isSaveBoardOpen.value = false
-// }
-
-
-// const copyAction = async (prompt: string) => {
-//   if (!checkAuthentication()) return;
-//   if (navigator.clipboard && typeof navigator.clipboard.writeText === 'function') {
-//     try {
-//       await navigator.clipboard.writeText(prompt)
-//       toastStore.success('Prompt copied to clipboard')
-//       console.log('Prompt copied to clipboard:', prompt)
-//     } catch (error) {
-//       console.error('Failed to copy prompt using Clipboard API:', error)
-//     }
-//   } else {
-//     console.warn('Clipboard API not supported, using fallback method')
-//     // Fallback method for unsupported environments
-//     const textArea = document.createElement('textarea')
-//     textArea.value = prompt
-//     textArea.style.position = 'fixed' // Avoid scrolling to the bottom
-//     document.body.appendChild(textArea)
-//     textArea.focus()
-//     textArea.select()
-//     toastStore.success('Prompt copied to clipboard')
-//     try {
-//       document.execCommand('copy')
-//       console.log('Prompt copied to clipboard using fallback')
-//     } catch (err) {
-//       console.error('Fallback: Unable to copy prompt:', err)
-//     }
-//     document.body.removeChild(textArea)
-//   }
-// }
-
-
-// const fetchMedia = async () => {
-//   loading.value = true
-//   media.value = null;
-//   try {
-//     const { data: response } = await genAiService.getCommunityMedia()
-
-//     if (response.status && Array.isArray(response.data)) {
-//       // Map data with type detection (image/video) for initial load
-//       media.value = response.data.map((item) => ({
-//         url: item.content,
-//         type: item.type,
-//         orientation: item.orientation,
-//         prompt: item.prompt,
-//         isLiked: item.like,
-//         isShared: item.share,
-//         owner: item.shareOwner,
-//         board: item.boardName
-//       }))
-//     } else {
-//       console.error('Failed to fetch images: Invalid response format')
-//     }
-//   } catch (error) {
-//     console.error('Error fetching images:', error)
-//   } finally {
-//     loading.value = false
-//   }
-// }
-
-
-
-// Fetch images when the component is mounted
-// onMounted(async () => {
-//   fetchMedia()
-// })
-
-// Tabs State
 const activeTab = ref('for-you')
-
-
-
-
-// Function to fetch liked media
-// const fetchLikedMedia = async (label) => {
-//   loading.value = true
-//   media.value = null;
-//   if (label === 'Text to Image') label = 'text-to-image'
-//   else if (label === 'Image to Image') label = 'image-to-image'
-//   else if (label === 'Text to Video') label = 'text-to-video'
-//   else if (label === 'Image to Video') label = 'image-to-video'
-//   else if (label === 'Face Swap') label = 'face-swap'
-//   else if (label === 'Templates (Beta)') label = 'templates'
-
-//   try {
-//     const { data: response } = await genAiService.getLikedMedia(label)
-
-//     if (response.status && Array.isArray(response.data)) {
-//       media.value = response.data.map((item) => ({
-//         url: item.content,
-//         type:
-//           item.type ||
-//           (label === 'text-to-video' ||
-//             label === 'image-to-video' ||
-//             label === 'templates' ||
-//             (label === 'face-swap' && item.orientation == null)
-//             ? 'video'
-//             : 'image'),
-//         orientation: item.orientation,
-//         prompt: item.prompt,
-//         isLiked: item.like,
-//         isShared: item.share,
-//         board: item.boardName || 'Board'
-//       }))
-//     } else {
-//       console.error('Failed to fetch images: Invalid response format')
-//     }
-//   } catch (error) {
-//     console.error('Error fetching images:', error)
-//   } finally {
-//     loading.value = false
-//   }
-// }
-
-
-// Alternatively, call it on mount if the default tab is 'favourites'
-onMounted(() => {
-
-})
-
 </script>
-
 <template>
   <DefaultLayout :showBadge="showBadge">
     <div class="flex flex-col h-full">
